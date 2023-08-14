d15fea8609b6997b4ac47d4a03a7784b8430fe88
Shubham Singh <shubhamsingh.exe@gmail.com>
2023-08-15 00:21:53 +0530

    Create 1491_avg_salary_ex_min_max.java

---
+++ Easy Questions/1491_avg_salary_ex_min_max.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        double sum = 0;
+        double len = salary.length - 2;
+        for(int i=1;i<salary.length-1;i++){
+            sum = sum + salary[i];
+        }
+        double avg = sum / len;
+        
+        return avg;
+    }
+}
